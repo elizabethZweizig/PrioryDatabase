ae14259f30ebd7af84ec04bd5db85e8fe7a34d94
Liana Bardallis <yastaghr@gmail.com>
2016-10-14 20:49:30 -0700

    thing change may work

--- gabeAndTheGaysDatabasesProject/updateTable.php
+++ gabeAndTheGaysDatabasesProject/updateTable.php
@@ -11,17 +11,17 @@ try{
 
   if ($table == 'passengers'){
 
-    $db->"update passengers set f_name = '$_POST[f_name]', m_name = '$_POST[m_name]', l_name = '$_POST[l_name]', ssn = '$_POST[ssn]' where ssn = $_POST['prim'];";
+    $db->"update passengers set f_name = '$_POST[f_name]', m_name = '$_POST[m_name]', l_name = '$_POST[l_name]', ssn = '$_POST[ssn]' where ssn = '$_POST[prim]';";
 
   }
   else if ($table == 'planes'){
 
-    $db->"update planes set tail_no = '$_POST[tail_no]', make = '$_POST[make]', model = '$_POST[model]', capacity = '$_POST[capicity]', mph = '$_POST[mph]' where tail_no = $_POST['prim'];";
+    $db->"update planes set tail_no = '$_POST[tail_no]', make = '$_POST[make]', model = '$_POST[model]', capacity = '$_POST[capicity]', mph = '$_POST[mph]' where tail_no = '$_POST[prim]';";
 
   }
   else if ($table == 'flights'){
 
-    $db->"update flights set flight_no='$_POST[flight_no]', dep_loc='$_POST[dep_loc]', dep_time='$_POST[dep_time]', arr_loc='$_POST[arr_loc]', arr_time='$_POST[arr_time]', tail_no='$_POST[tail_no]' where flight_no='$_POST['prim']';";
+    $db->"update flights set flight_no='$_POST[flight_no]', dep_loc='$_POST[dep_loc]', dep_time='$_POST[dep_time]', arr_loc='$_POST[arr_loc]', arr_time='$_POST[arr_time]', tail_no='$_POST[tail_no]' where flight_no='$_POST[prim]';";
 
   }
   else if ($table == 'onboard'){
