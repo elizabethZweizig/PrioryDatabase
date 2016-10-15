df1e0be6bb693a1037620d4e984b0a583e6f809b
Liana Bardallis <yastaghr@gmail.com>
2016-10-14 20:57:00 -0700

    okay this needs a query idiot me

--- gabeAndTheGaysDatabasesProject/updateTable.php
+++ gabeAndTheGaysDatabasesProject/updateTable.php
@@ -11,24 +11,24 @@ try{
 
   if ($table == 'passengers'){
 
-    $db->"update passengers set f_name = '$_POST[f_name]', m_name = '$_POST[m_name]', l_name = '$_POST[l_name]', ssn = '$_POST[ssn]' where ssn = '$_POST[prim]';";
+    $db->query("update passengers set f_name = '$_POST[f_name]', m_name = '$_POST[m_name]', l_name = '$_POST[l_name]', ssn = '$_POST[ssn]' where ssn = '$_POST[prim]';");
 
   }
   else if ($table == 'planes'){
 
-    $db->"update planes set tail_no = '$_POST[tail_no]', make = '$_POST[make]', model = '$_POST[model]', capacity = '$_POST[capicity]', mph = '$_POST[mph]' where tail_no = '$_POST[prim]';";
+    $db->query("update planes set tail_no = '$_POST[tail_no]', make = '$_POST[make]', model = '$_POST[model]', capacity = '$_POST[capicity]', mph = '$_POST[mph]' where tail_no = '$_POST[prim]';");
 
   }
   else if ($table == 'flights'){
 
-    $db->"update flights set flight_no='$_POST[flight_no]', dep_loc='$_POST[dep_loc]', dep_time='$_POST[dep_time]', arr_loc='$_POST[arr_loc]', arr_time='$_POST[arr_time]', tail_no='$_POST[tail_no]' where flight_no='$_POST[prim]';";
+    $db->query("update flights set flight_no='$_POST[flight_no]', dep_loc='$_POST[dep_loc]', dep_time='$_POST[dep_time]', arr_loc='$_POST[arr_loc]', arr_time='$_POST[arr_time]', tail_no='$_POST[tail_no]' where flight_no='$_POST[prim]';");
 
   }
   else if ($table == 'onboard'){
     //double primary key
     $ssnPrim = $_POST['prim1'];
     $flight_noPrim = $_POST['prim2'];
-    $db->"update onboard set ssn='$_POST[ssn]', flight_no='$_POST[flight_no]', seat='$_POST[seat]' where ssn=$ssnPrim, flight_no=$flight_noPrim;";
+    $db->query("update onboard set ssn='$_POST[ssn]', flight_no='$_POST[flight_no]', seat='$_POST[seat]' where ssn=$ssnPrim, flight_no=$flight_noPrim;");
   }
   else {
     die('Exception : Table not valid.');
