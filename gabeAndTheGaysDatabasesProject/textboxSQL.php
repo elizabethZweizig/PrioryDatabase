ecf9dca00016051ea6467e30e13dc1cb7ae5f113
rvhirsch <rhirsch@pugetsound.edu>
2016-10-17 17:30:02 -0700

    THIS IS ALL WORKING NOW

--- gabeAndTheGaysDatabasesProject/textboxSQL.php
+++ gabeAndTheGaysDatabasesProject/textboxSQL.php
@@ -4,9 +4,10 @@
 <title>Input An SQL Query</title>
 <h3>Input An SQL Query</h3>
 <?php
+ini_set('display_errors', 'Off'); // gets rid of fetchAll() fatal error, which isnt even an issue???
 
 echo "<form method='get'>";
-echo "<input type='text' name='command' placeholder='Ex: select * from planes;'$
+echo "<input type='text' name='command' placeholder='Ex: select * from planes;'/>";
 
 echo "<input type='submit' value='Submit Query'/>";
 echo "</form>";
@@ -16,56 +17,57 @@ try
                         //open the sqlite database file
                         $db = new PDO('sqlite:./database/airport.db');
                         // Set errormode to exceptions
-                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEP$
+                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                        $value = isset($_GET['command']) ? $_GET['command'] : '$
-
-                        echo $value;            // for testing only
-                        echo "<br>";
-                        //$value = "\'".$value."\'";
-                        //echo $value;
-                        //echo "<br>";
+                        $value = isset($_GET['command']) ? $_GET['command'] : '';
 
                         $result = $db->query($value,PDO::FETCH_ASSOC);
 
-                       $array = $result->fetchAll();
-//                      print_r($array);        // for testing only
-//                      echo "<br>";
+                        $array = $result->fetchAll();
 
+                        print_r('Your Query: ' . $value . "<br><br>");
+                        print_r('Result:' . "<br>");
 
-                       echo "<table border='1'>";
-                       $i = 0;
-                       foreach ($array as $tuple)
-                       {
-//                              print_r($tuple);        // for testing only
-//                              echo "<br>";
-                               if ($i == "0") {
-                                       echo "<tr style='font-weight:bold'>";
-                                       foreach ($tuple as $key => $value)
-                                       {
-                                               echo "<td>";
-                                               print_r($key);
-                                               echo "</td>";
-                                       }
-                                       echo "</tr>";
-                               }
-                               echo "<tr>";
-                               foreach ($tuple as $key => $value)
-                               {
-                                       echo "<td>";
-                                       print_r($value);
-                                       echo "</td>";
-                               }
-                               echo "</tr>";
-                               $i++;
-                       }
-                       echo "</table>";
-               }
-
+                        if (strpos(strtolower($value), 'update') !== false)
+                        {
+                                print_r('Table Updated');
+                        }
+                        else if (strpos(strtolower($value), 'delete') !== false)
+                        {
+                                print_r('Table Info Deleted');
+                        }
+                        else
+                        {
+                                echo "<table border='1'>";
+                                $i = 0;
+                                foreach ($array as $tuple)
+                                {
+                                        if ($i == "0") {
+                                                echo "<tr style='font-weight:bold'>";
+                                                foreach ($tuple as $key => $value)
+                                                {
+                                                        echo "<td>";
+                                                        print_r($key);
+                                                        echo "</td>";
+                                                }
+                                                echo "</tr>";
+                                        }
+                                        echo "<tr>";
+                                        foreach ($tuple as $key => $value)
+                                        {
+                                                echo "<td>";
+                                                print_r($value);
+                                                echo "</td>";
+                                        }
+                                        echo "</tr>";
+                                        $i++;
+                                }
+                                echo "</table>";
+                        }
+                }
 catch(PDOException $e)
                 {
                         die('Exception : '.$e->getMessage());
                 }
 ?>
 </body>
-</html>
