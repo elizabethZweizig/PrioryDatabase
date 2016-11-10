806072ff2329851167ecc5aa7a19bc48f24930e4
rvhirsch <rhirsch@pugetsound.edu>
2016-11-10 15:58:02 -0800

    changed names of some attrs to not be 'date'

--- gabeAndTheGaysDatabasesProject/prioryProject/priorySchema.sql
+++ gabeAndTheGaysDatabasesProject/prioryProject/priorySchema.sql
@@ -23,12 +23,12 @@ CREATE TABLE equipment (
 CREATE TABLE equipRes (
   equipID INTEGER,
   pID TEXT,
-  date TEXT,    -- need to specify "YYYY-MM-DD" format ??
+  dateUsed TEXT,    -- need to specify "YYYY-MM-DD" format ??
   timeIn TEXT,    -- need to specify "HH:MM" format ??
   timeOut TEXT,    -- need to specify "HH:MM" format ??
-  FOREIGN KEY (equipID) REFERENCES equipment(equipID) ON DELETE CASCADE ON UPDATE CASCADE, 
-  FOREIGN KEY (pID) REFERENCES login(pID)ON DELETE CASCADE ON UPDATE CASCADE, 
-  PRIMARY KEY (equipID, date, timeIn)
+  FOREIGN KEY (equipID) REFERENCES equipment(equipID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (pID) REFERENCES login(pID)ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (equipID, dateUsed, timeIn)
 )
 
 CREATE TABLE bedroom (
@@ -39,30 +39,30 @@ CREATE TABLE bedroom (
 
 CREATE TABLE bedRes (
   pID TEXT,
-  bedID TEXT,   
+  bedID TEXT,
   numPpl INTEGER CHECK(bedRes(numPpl) <= bedroom(maxPpl)), --check numppl is less than max in room
   checkIn TEXT,   -- need to specify "YYYY-MM-DD" format ??
   checkOut TEXT,   -- need to specify "YYYY-MM-DD" format ??
-  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE, 
-  FOREIGN KEY (bedID) REFERENCES bedroom(bedID) ON DELETE CASCADE ON UPDATE CASCADE, 
+  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (bedID) REFERENCES bedroom(bedID) ON DELETE CASCADE ON UPDATE CASCADE,
   PRIMARY KEY (bedID, checkIn)
 )
 
 CREATE TABLE meetRoom (
-  roomID TEXT PRIMARY KEY, 
+  roomID TEXT PRIMARY KEY,
   maxPpl INTEGER,
   meetRate REAL --how much to rent the room
 )
 
 CREATE TABLE meetRes (
-  roomID TEXT, 
+  roomID TEXT,
   pID TEXT,
   numPpl INTEGER CHECK(meetRes(numPpl) <= meetRoom(maxPpl)), --check peple thing
   dateUsed TEXT,    -- need to specify "YYYY-MM-DD" format ??
   timeIn TEXT,    -- need to specify "HH:MM" format ??
   timeOut TEXT,    -- need to specify "HH:MM" format ??
   FOREIGN KEY (roomID) REFERENCES meetRoom(roomID) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,  
+  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
   PRIMARY KEY (roomID, dateUsed, timeIn)
 )
 
@@ -71,18 +71,18 @@ CREATE TABLE program (
   contact INTEGER, --person in charge
   roomID TEXT, --room they are in (might be more that one?)
   cost REAL,
-  date TEXT,
+  dateUsed TEXT,
   timeIn TEXT,    -- need to specify "HH:MM" format ??
   timeOut TEXT,    -- need to specify "HH:MM" format ??
   FOREIGN KEY (contact) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (roomID) REFERENCES meetRoom(roomID) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (programName, contact, date, timeIn) 
+  PRIMARY KEY (programName, contact, dateUsed, timeIn) 
 )
 
 CREATE TABLE programRoster ( --think onboard table
   programName TEXT, --what they are attending
   pID, --person attending program
-  FOREIGN KEY (programName) REFERENCES program(programName) ON DELETE CASCADE ON UPDATE CASCADE,  
-  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,  
+  FOREIGN KEY (programName) REFERENCES program(programName) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
   PRIMARY KEY (programName, pID)
 )
