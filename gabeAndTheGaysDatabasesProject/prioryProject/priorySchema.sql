12017d1787731846914b59c620e0d64fd03f1b63
rvhirsch <rhirsch@pugetsound.edu>
2016-11-19 14:41:32 -0800

    NEW TABLES AND SHIT

--- gabeAndTheGaysDatabasesProject/prioryProject/priorySchema.sql
+++ gabeAndTheGaysDatabasesProject/prioryProject/priorySchema.sql
@@ -15,6 +15,17 @@ CREATE TABLE login (
   admin INTEGER   -- 1 if true, 0 if false
 );
 
+CREATE TABLE atPriory (
+  pID INTEGER,
+  checkIn TEXT CHECK(GLOB('????-??-??', checkIn)),
+  checkOut TEXT CHECK(GLOB('????-??-??', checkOut)),
+  timeIn TEXT CHECK(GLOB('??:??', timeIn)),
+  timeOut TEXT CHECK(GLOB('??:??', timeOut)),
+  dateRecvd TEXT CHECK(GLOB('????-??-??', dateRecvd)),
+  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (pID, checkIn)
+);
+
 CREATE TABLE equipment (
   equipID INTEGER PRIMARY KEY,
   type TEXT, --what the thing is
@@ -23,14 +34,13 @@ CREATE TABLE equipment (
 );
 
 CREATE TABLE equipRes (
+  equipResID INTEGER PRIMARY KEY,
+  roomReq INTEGER,  -- 1 if room is required
   equipID INTEGER,
-  pID INTEGER,
   dateUsed TEXT CHECK(GLOB('????-??-??', dateUsed)),
   timeIn TEXT CHECK(GLOB('??:??', timeIn)),
   timeOut TEXT CHECK(GLOB('??:??', timeOut)),
-  FOREIGN KEY (equipID) REFERENCES equipment(equipID) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (pID) REFERENCES login(pID)ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (equipID, dateUsed, timeIn)
+  FOREIGN KEY (equipID) REFERENCES equipment(equipID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE bedroom (
@@ -40,7 +50,7 @@ CREATE TABLE bedroom (
 );
 
 CREATE TABLE bedRes (
-  pID INTEGER,
+  bedResID INTEGER PRIMARY KEY,
   bedID TEXT,
   numPpl INTEGER CHECK (numPpl >= 0),
   --  CHECK (bedRes(numPpl) <= bedroom(maxPpl)), -- check numppl is less than max in room - do this in html??
@@ -49,9 +59,7 @@ CREATE TABLE bedRes (
   timeIn TEXT CHECK(GLOB('??:??', timeIn)),
   timeOut TEXT CHECK(GLOB('??:??', timeOut)),
   dateRecvd TEXT CHECK(GLOB('????-??-??', dateRecvd)),
-  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (bedID) REFERENCES bedroom(bedID) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (bedID, checkIn)
+  FOREIGN KEY (bedID) REFERENCES bedroom(bedID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE meetRoom (
@@ -62,40 +70,84 @@ CREATE TABLE meetRoom (
 );
 
 CREATE TABLE meetRes (
+  meetResID INTEGER PRIMARY KEY,
   roomID TEXT,
-  pID INTEGER,
   numPpl INTEGER CHECK (numPpl >= 0),
   -- CHECK(meetRes(numPpl) <= meetRoom(maxPpl)), --check peple thing
   dateUsed TEXT CHECK(GLOB('????-??-??', dateUsed)),
   timeIn TEXT CHECK(GLOB('??:??', timeIn)),
   timeOut TEXT CHECK(GLOB('??:??', timeOut)),
   dateRecvd TEXT CHECK(GLOB('????-??-??', dateRecvd)),
-  FOREIGN KEY (roomID) REFERENCES meetRoom(roomID) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (roomID, dateUsed, timeIn)
+  FOREIGN KEY (roomID) REFERENCES meetRoom(roomID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE program (
-  programName TEXT,
-  contact INTEGER, --person in charge of program - NOT pID
-  roomID TEXT, --room they are in (might be more than one?)
-  cost REAL CHECK (cost >= 0),
-  dateUsed TEXT CHECK(GLOB('????-??-??', dateUsed)),
-  timeIn TEXT CHECK(GLOB('??:??', timeIn)),
-  timeOut TEXT CHECK(GLOB('??:??', timeOut)),
-  tour INTEGER, -- 1 for true, 0 for false
-  notes TEXT,
-  FOREIGN KEY (contact) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (roomID) REFERENCES meetRoom(roomID) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (programName, contact, dateUsed, timeIn)
+CREATE TABLE groupInfo (
+  groupID INTEGER PRIMARY KEY,
+  contactPerson INTEGER,
+  groupName TEXT,
+  groupNeeds TEXT,
+  tour INTEGER,
+  FOREIGN KEY (contactPerson) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE programRoster ( -- think onboard table
-  programName TEXT, -- what program they are attending
-  pID INTEGER, -- pID of contactperson for group attending program
-  numPpl INTEGER, -- number of people associated with group
-  dateRecvd TEXT CHECK(GLOB('????-??-??', dateRecvd)),
-  FOREIGN KEY (programName) REFERENCES program(programName) ON DELETE CASCADE ON UPDATE CASCADE,
+CREATE TABLE dayVisit (
+  pID INTEGER,
+  day TEXT CHECK(GLOB('????-??-??', day)),
+  tour INTEGER,
   FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (programName, pID)
+  PRIMARY KEY (pID, day)
+);
+
+CREATE TABLE nightVisit (
+  pID INTEGER,
+  day TEXT CHECK(GLOB('????-??-??', day)),
+  bedResID INTEGER,
+  FOREIGN KEY (bedResID) REFERENCES bedRes(bedResID) ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (pID, day)
+);
+
+CREATE TABLE groupRooms (
+  groupID INTEGER,
+  meetResID INTEGER,
+  FOREIGN KEY (groupID) REFERENCES groupInfo(groupID)  ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (meetResID) REFERENCES meetRes(meetResID)  ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (groupID, meetResID)
+);
+
+CREATE TABLE prioryEvent (
+  eventID INTEGER PRIMARY KEY,
+  startDate TEXT,
+  endDate TEXT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE eventRoom (
+  eventID INTEGER,
+  meetResID INTEGER,
+  FOREIGN KEY (eventID) REFERENCES prioryEvent(eventID)  ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (meetResID) REFERENCES meetRes(meetResID)  ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (eventID, meetResID)
+);
+
+CREATE TABLE eventRoster (
+  eventID INTEGER,
+  pID INTEGER,
+  numPpl INTEGER,
+  FOREIGN KEY (eventID) REFERENCES prioryEvent(eventID)  ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (pID) REFERENCES login(pID) ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY (eventID, pID)
+);
+
+CREATE TABLE groupOvernight (
+  overnightID INTEGER PRIMARY KEY,
+  groupID INTEGER,
+  FOREIGN KEY (groupID) REFERENCES groupInfo(groupID)  ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE overnightBeds (
+  overnightID INTEGER,
+  bedResID INTEGER,
+  FOREIGN KEY (overnightID) REFERENCES groupOvernight(overnightID)  ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (bedResID) REFERENCES bedRes(bedResID)  ON DELETE CASCADE ON UPDATE CASCADE
 );
