8758375d06d582a153f8bfe1da0b4f0bf7e76229
yastaghr <yastaghr@gmail.com>
2016-11-29 19:23:04 -0800

    individualOvernight, p1

--- gabeAndTheGaysDatabasesProject/prioryProject/individualOvernightRegister.php
+++ gabeAndTheGaysDatabasesProject/prioryProject/individualOvernightRegister.php
@@ -1,94 +1,136 @@
 <!--TODO: fix all the things to be for indivreg-->
 
 <?php
-        if (empty($_POST['contact']))
-        {
-                //send back to sign in page
-                header('Location: '.signin.html);
-                die();
-        }
+if (empty($_POST['person']))
+{
+  //send back to sign in page
+  header('Location: '.signin.html);
+  die();
+}
 
-        if (empty($_POST['numPpl']))
-        {
-                //send back
-                header('Location: '.$individualOvernightRegistration.php);
-                die();
-        }
-        if (empty($_POST['checkInDate']))
-        {
-                //send back
-                header('Location: '.$individualOvernightRegistration.php);
-                die();
-        }
-        if (empty($_POST['checkOutDate']))
-        {
-                //send back
-                header('Location: '.$individualOvernightRegistration.php);
-                die();
-        }
-        if (empty($_POST['checkInTime']))
-        {
-                //send back
-                header('Location: '.$individualOvernightRegistration.php);
-                die();
-        }
-        if (empty($_POST['checkOutTime']))
-        {
-                //send back
-                header('Location: '.$individualOvernightRegistration.php);
-                die();
-        }
-        if (empty($_POST['dateRecvd']))
-        {
-                //send back
-                header('Location: '.$individualOvernightRegistration.php);
-                die();
-        }
-        if (empty($_POST['tour']))
-        {
-                //send back
-                header('Location: '.$individualOvernightRegistration.php);
-                die();
-        }
-        else {
-                try
-            {
-                //open the sqlite database file
-                $db = new PDO('sqlite:./database/priorydb.db');
+if (empty($_POST['numPpl']))
+{
+  //send back
+  header('Location: '.$individualOvernightRegistration.php);
+  die();
+}
+if (empty($_POST['checkIn']))
+{
+  //send back
+  header('Location: '.$individualOvernightRegistration.php);
+  die();
+}
+if (empty($_POST['checkOut']))
+{
+  //send back
+  header('Location: '.$individualOvernightRegistration.php);
+  die();
+}
+if (empty($_POST['timeIn']))
+{
+  //send back
+  header('Location: '.$individualOvernightRegistration.php);
+  die();
+}
+if (empty($_POST['timeOut']))
+{
+  //send back
+  header('Location: '.$individualOvernightRegistration.php);
+  die();
+}
+if (empty($_POST['dateRecvd']))
+{
+  //send back
+  header('Location: '.$individualOvernightRegistration.php);
+  die();
+}
+if (empty($_POST['tour']))
+{
+  //send back
+  header('Location: '.$individualOvernightRegistration.php);
+  die();
+}
+else {
+  try
+  {
+    //open the sqlite database file
+    $db = new PDO('sqlite:./database/priorydb.db');
 
-                // Set errormode to exceptions
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-                // sets vars = POST variables
-                $contact = $_POST['contact'];
-                $numPpl = $_POST['numPpl'];
-                $checkInDate = $_POST['checkInDate'];
-                $checkOutDate = $_POST['checkOutDate'];
-                $checkInTime = $_POST['checkInTime'];
-                $checkOutTime = $_POST['checkOutTime'];
-                $dateRecvd = $_POST['dateRecvd'];
-                $tour = $_POST['tour'];
+    // Set errormode to exceptions
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+    // sets vars = POST variables
+    $person = $_POST['person'];
+    $numPpl = $_POST['numPpl'];
+    $checkIn = $_POST['checkIn'];
+    $checkOut = $_POST['checkOut'];
+    $timeIn = $_POST['timeIn'];
+    $timeOut = $_POST['timeOut'];
+    $dateRecvd = $_POST['dateRecvd'];
+    $tour = $_POST['tour'];
 
-                // prepares the sql statement
-								$sqlStatement = $db->prepare("insert into passengers (f_name, m_name, l_name, ssn) values (:f_name, :m_name, :l_name, :ssn); ");
+    //prepares first sql (bedRes)
+    $sqlStatement = $db->prepare(
+      "insert into bedRes (
+        bedResID,
+        bedId,
+        numPpl,
+        checkIn,
+        checkOut,
+        timeIn,
+        timeOut,
+        dateRecvd)
+        values (
+          NULL,
+          NULL,
+          :numPpl,
+          :checkIn,
+          :checkOut,
+          :timeIn,
+          :timeOut,
+          :dateRecvd
+        );"
+      );
 
-                // binds parameters to be used in sql statement
-                $sqlStatement->bindParam(':f_name', $firstname);
-                $sqlStatement->bindParam(':m_name', $midname);
-                $sqlStatement->bindParam(':l_name', $lastname);
-                $sqlStatement->bindParam(':ssn', $socialsec);
-								//executes statement
-                $sqlStatement->execute();
-				        //$db->exec("insert into passengers values ('$_POST[f_name]', '$_POST[m_name]', '$_POST[l_name]', '$_POST[ssn]');");
-				        }
-				    catch(PDOException $e)
-				    {
-				        die('Exception : '.$e->getMessage());
-				    }
-				}
-				// sends to success page
-				header("Location: success.html");
-?>
+      // prepares the sql statement
+      //$sqlStatement = $db->prepare("insert into passengers (f_name, m_name, l_name, ssn) values (:f_name, :m_name, :l_name, :ssn); ");
+
+      // binds parameters to be used in sql statement
+      $sqlStatement->bindParam(':numPpl', $numPpl);
+      $sqlStatement->bindParam(':checkIn', $checkIn);
+      $sqlStatement->bindParam(':checkOut', $checkOut);
+      $sqlStatement->bindParam(':timeIn', $timeIn);
+      $sqlStatement->bindParam(':timeOut', $timeOut);
+      $sqlStatement->bindParam(':dateRecvd', $dateRecvd);
+      //executes statement
+      $sqlStatement->execute();
+      //$db->exec("insert into passengers values ('$_POST[f_name]', '$_POST[m_name]', '$_POST[l_name]', '$_POST[ssn]');");
+
+      $bedResID = 0; //TODO: Rachel help get bedResID of tuple just created
+
+      //for every day being stayed
+      $oneDay = 8.64e7;
+      //TODO: make this work (is js)
+      for ($i = $checkIn.getTime(); i<=$checkOut.getTime(); $i+=$oneDay){
+        $sqlNight = $db->prepare('insert into nightVisit values (:person, :day, :bedResID);');
+        $sqlNight->bindParam(':person', $person);
+        $sqlNight->bindParam(':checkIn', $i); //TODO: fix wrong format
+        $sqlNight->bindParam(':bedResID', $bedResID);
+        $sqlNight->execute();
+
+        $sqlDay = $db->prepare('insert into nightVisit values (:person, :day, :tour);');
+        $sqlDay->bindParam(':tour', $tour);
+        $sqlDay->execute();
+      }
+
+      //TODO: notify admin of need to schedule beds. include bedResID
+    }
+    catch(PDOException $e)
+    {
+      die('Exception : '.$e->getMessage());
+    }
+  }
+  // sends to success page
+  header("Location: success.html");
+  ?>
